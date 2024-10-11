9ee03d0e079e52349dc0e43cb4ceac751f4c8718
reklast <reklast51@gmail.com>
2024-10-11 19:01:50 +0300

    feat: userCard component implemented

---
+++ src/componentsShared/UserCard/UserCard.tsx
@@ -0,0 +1,24 @@
+import avatarPlaceholder from "../../assets/avatarPlaceholder.png";
+import { User } from "../../types/users";
+
+import styles from "./UserCard.module.scss";
+
+function UserCard({ user }: { user: User }) {
+  return (
+    <div className={styles.cardWrap}>
+      <img src={user.avatar ?? avatarPlaceholder} alt="userAvatar" />
+      <div className={styles.userInfo}>
+        <div className={styles.name}>{user.name}</div>
+        <ul>
+          <li>Email: {user.email}</li>
+          <li>Phone: {user.phone}</li>
+          <li>Website: {user.website}</li>
+          Address: {user.address.street}, {user.address.suite},{" "}
+          {user.address.city}, {user.address.zipcode}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default UserCard;
