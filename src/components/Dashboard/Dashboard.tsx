efa55aead1709c472ce9cddc86c87cb84d83eb92
reklast <reklast51@gmail.com>
2024-10-11 19:07:58 +0300

    feat: implemented user data rendering inside Dashboard component

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,13 +1,37 @@
+import { useEffect, useState } from "react";
+import { User } from "../../types/users";
+import { getUsers } from "../../services/users";
 
 import Header from "../../componentsShared/Header/Header";
+import UserCard from "../../componentsShared/UserCard/UserCard";
+
 import styles from "./Dashboard.module.scss";
 
 function Dashboard() {
+  const [users, setUsers] = useState<User[] | undefined>(undefined);
+
+  useEffect(() => {
+    getUsers().then((data) => setUsers(data));
+  }, []);
+
   return (
     <div className={styles.dashboardWrap}>
-        <Header name="Dashboard" />
+      <Header name="Dashboard" />
+      <div className={styles.mainBlock}>
+        {!users ? (
+          "Loading..."
+        ) : (
+          <>
+            <div className={styles.userCards}>
+              {users.map((user, i) => (
+                <UserCard key={i} user={user} />
+              ))}
+            </div>
+          </>
+        )}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;
