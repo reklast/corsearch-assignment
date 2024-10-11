163a159f3065dd23f83e559ca5ff46cce6d9555d
reklast <reklast51@gmail.com>
2024-10-11 18:33:49 +0300

    feat: type for user object added

---
+++ src/types/users.ts
@@ -0,0 +1,14 @@
+export type User = {
+  id: number;
+  avatar: string;
+  name: string;
+  email: string;
+  phone: string;
+  website: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+  };
+};
